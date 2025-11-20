3b87312e3dc22017701fc664c0db97398c85e806
20055568 <lohithbadam10@gmail.com>
2025-11-20 10:11:20 +0000

    Update api.js

--- test/api.js
+++ test/api.js
@@ -24,8 +24,5 @@ describe('API Basic Test', () => {
     expect(response.statusCode).toBe(200);
     expect(Array.isArray(response.body)).toBe(true);
   });
-
-  afterraset(async => {
-    cinst raw 
                                             
 
